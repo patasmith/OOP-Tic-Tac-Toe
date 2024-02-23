f6e4085ccb724c4ac95d83641ea735e7cf0176ae
Pat Smith <pat.a.smith.dev@gmail.com>
2024-02-22 22:01:57 -0500

    changed styles; each square has an X svg

--- view.js
+++ view.js
@@ -5,6 +5,27 @@ class Grid {
     this.#elements = this.#makeGrid(size);
   }
 
+  #createLine(x1, y1, x2, y2) {
+    let line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line.setAttribute("x1", x1);
+    line.setAttribute("y1", y1);
+    line.setAttribute("x2", x2);
+    line.setAttribute("y2", y2);
+    line.setAttribute("stroke", "black");
+    line.setAttribute("stroke-width", 2);
+    return line;
+  }
+
+  #createXMark() {
+    const mark = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    mark.setAttribute("viewBox", "0 0 100 100");
+    mark.setAttribute("width", "100");
+    mark.setAttribute("height", "100");
+    mark.appendChild(this.#createLine(10, 10, 90, 90));
+    mark.appendChild(this.#createLine(90, 10, 10, 90));
+    return mark
+  }
+
   #createDiv(className) {
     const row = document.createElement("div");
     row.className = className;
@@ -17,6 +38,7 @@ class Grid {
     for (let i = 0; i < size ** 2; i++) {
       const square = this.#createDiv("square");
       square.id = i;
+      square.appendChild(this.#createXMark());
       wrapper.appendChild(square);
     }
     return wrapper;
